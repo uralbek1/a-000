3771b11929e45bc29a3b19b6a172e6f1802557b2
uralbek1 <162686008+uralbek1@users.noreply.github.com>
2024-11-10 05:14:29 +0500

    update misol.js

--- misol.js
+++ misol.js
@@ -1,1 +1,5 @@
 const text = '["Uralbek UZ", "Onlayn xususiy baza"]'
+const myArr = JSON.parse(text);
+document.getElementById("dom1").innerHTML = myArr[1];
+const myArr = JSON.parse(text);
+document.getElementById("d").innerHTML = myArr[1];
